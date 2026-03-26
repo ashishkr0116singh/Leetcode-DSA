6b99702641c20b4e7aeceeb17b144f554294a68b
Ashish kumar singh <Ashish.24214@stu.upes.ac.in>
2026-03-26 16:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (15.66%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.c
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ */
+int** zigzagLevelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    if (!root) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+
+    // Queue for BFS
+    struct TreeNode* queue[2000];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    // Result arrays
+    int** result = (int**)malloc(sizeof(int*) * 2000);
+    *returnColumnSizes = (int*)malloc(sizeof(int) * 2000);
+    *returnSize = 0;
+
+    int leftToRight = 1; // flag to alternate direction
+
+    while (front < rear) {
+        int levelSize = rear - front;
+        int* level = (int*)malloc(sizeof(int) * levelSize);
+
+        for (int i = 0; i < levelSize; i++) {
+            struct TreeNode* node = queue[front++];
+            // Place values depending on direction
+            if (leftToRight) {
+                level[i] = node->val;
+            } else {
+                level[levelSize - 1 - i] = node->val;
+            }
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+
+        result[*returnSize] = level;
+        (*returnColumnSizes)[*returnSize] = levelSize;
+        (*returnSize)++;
+        leftToRight = !leftToRight; // flip direction
+    }
+
+    return result;
+}
