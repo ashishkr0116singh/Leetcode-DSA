034a60f842861b3ad445cf93deb2d2e570c08cad
Ashish kumar singh <Ashish.24214@stu.upes.ac.in>
2026-04-04 22:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (33.16%) - LeetSync

---
+++ 733-flood-fill/flood-fill.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void dfs(int** image, int imageSize, int* imageColSize,
+         int sr, int sc, int newColor, int oldColor) {
+    if (sr < 0 || sr >= imageSize || sc < 0 || sc >= imageColSize[sr]) return;
+    if (image[sr][sc] != oldColor) return;
+
+    image[sr][sc] = newColor;
+
+    dfs(image, imageSize, imageColSize, sr+1, sc, newColor, oldColor);
+    dfs(image, imageSize, imageColSize, sr-1, sc, newColor, oldColor);
+    dfs(image, imageSize, imageColSize, sr, sc+1, newColor, oldColor);
+    dfs(image, imageSize, imageColSize, sr, sc-1, newColor, oldColor);
+}
+
+int** floodFill(int** image, int imageSize, int* imageColSize,
+                int sr, int sc, int newColor, int* returnSize, int** returnColumnSizes) {
+    *returnSize = imageSize;
+    *returnColumnSizes = imageColSize;
+
+    int oldColor = image[sr][sc];
+    if (oldColor != newColor) {
+        dfs(image, imageSize, imageColSize, sr, sc, newColor, oldColor);
+    }
+    return image;
+}
