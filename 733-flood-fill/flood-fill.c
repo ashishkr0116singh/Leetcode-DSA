aaf128cb79333d13dcc4771bf734894cf5dbcdf7
Ashish kumar singh <Ashish.24214@stu.upes.ac.in>
2026-04-05 22:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (17.99%) - LeetSync

--- 733-flood-fill/flood-fill.c
+++ 733-flood-fill/flood-fill.c
@@ -4,11 +4,15 @@
 
 void dfs(int** image, int imageSize, int* imageColSize,
          int sr, int sc, int newColor, int oldColor) {
+    // Boundary check
     if (sr < 0 || sr >= imageSize || sc < 0 || sc >= imageColSize[sr]) return;
+    // Stop if pixel is not the old color
     if (image[sr][sc] != oldColor) return;
 
+    // Recolor the pixel
     image[sr][sc] = newColor;
 
+    // Explore neighbors
     dfs(image, imageSize, imageColSize, sr+1, sc, newColor, oldColor);
     dfs(image, imageSize, imageColSize, sr-1, sc, newColor, oldColor);
     dfs(image, imageSize, imageColSize, sr, sc+1, newColor, oldColor);
